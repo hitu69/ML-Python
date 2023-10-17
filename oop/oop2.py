5c2886f529c0e46414d4cc99c5a03828638b4b12
hitu69 <hiteshmarwaha24@gmail.com>
2023-10-17 14:16:21 +0530

    oop2

---
+++ oop/oop2.py
@@ -0,0 +1,42 @@
+# class variables
+
+class Employee: 
+  
+  num_of_emps=0
+  raise_amount = 1.04
+  
+  def __init__(self, first, last, pay):
+    self.first = first
+    self.last = last
+    self.pay = pay
+    self.email = first + '.' + last + '@gmail.com'
+    Employee.num_of_emps += 1
+  
+  def fullname(self): 
+    return '{} {}'.format(self.first, self.last)
+  
+  def apply_raise(self):
+    self.pay = int(self.pay * self.raise_amount)
+
+
+emp_1 = Employee('Hitesh', 'Marwaha', 50000)
+emp_2 = Employee('Test', 'User', 60000)
+
+# print(emp_1.pay)
+# emp_1.apply_raise()
+# print(emp_1.pay)
+
+
+
+# print(Employee.__dict__)
+
+# emp_1.raise_amount = 1.05
+
+# print(emp_1.__dict__)
+
+print(Employee.raise_amount)
+print(emp_1.raise_amount)
+print(emp_2.raise_amount)
+
+
+print(Employee.num_of_emps)
---
+++ oop/tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+print(Employee.raise_amount)
+print(emp_1.raise_amount)
+print(emp_2.raise_amount)
